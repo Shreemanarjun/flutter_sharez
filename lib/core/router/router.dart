c06c21a3f20376d0e70216daa513143de5d966b3
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-08-10 19:39:33 +0530

    🎨 import

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter_sharez/core/router/router.gr.dart';
-import 'package:flutter_sharez/features/receive/view/receive_state_page.dart';
 
 /// This class used for defined routes and paths na dother properties
 @AutoRouterConfig()
