6b27f367a91cdf9779de9eeb5ae39ad49970833f
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-08-10 11:41:11 +0530

    📱 fix responsive on tablet size

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -51,9 +51,9 @@ class _AppState extends ConsumerState<App> with GlobalHelper {
             minWidth: 450,
             defaultScale: true,
             breakpoints: [
-              const ResponsiveBreakpoint.resize(350, name: MOBILE),
-              const ResponsiveBreakpoint.resize(600, name: TABLET),
-              const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+              const ResponsiveBreakpoint.resize(480, name: MOBILE),
+              const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+              const ResponsiveBreakpoint.autoScaleDown(1200, name: DESKTOP),
               const ResponsiveBreakpoint.autoScaleDown(1700, name: 'XL'),
             ],
             background: Container(
