14ccc7095c6583d6867b150e2ba82b9f8d3b662f
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-08-20 19:58:44 +0530

    ✨ add device preview

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flash/flash_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -29,6 +30,7 @@ class _AppState extends ConsumerState<App> with GlobalHelper {
     final approuter = ref.watch(autorouterProvider);
     final currentTheme = ref.watch(themecontrollerProvider);
     final locale = ref.watch(localePod);
+
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Fshare App by Riverpod 💙',
@@ -42,13 +44,15 @@ class _AppState extends ConsumerState<App> with GlobalHelper {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      locale: locale,
+      locale: DevicePreview.locale(context) ?? locale,
       builder: (context, child) {
-        if (mounted) {
+        if (context.mounted) {
+          child = DevicePreview.appBuilder(context, child);
+
           ///Used for responsive design
           ///Here you can define breakpoint and how the responsive should work
           child = child = ResponsiveWrapper.builder(
-            BouncingScrollWrapper.builder(context, child!),
+            BouncingScrollWrapper.builder(context, child),
             maxWidth: 1700,
             minWidth: 450,
             defaultScale: true,
@@ -65,7 +69,7 @@ class _AppState extends ConsumerState<App> with GlobalHelper {
           final mediaquery = MediaQuery.of(context);
           child = MediaQuery(
             data: mediaquery.copyWith(
-              textScaleFactor: mediaquery.textScaleFactor.clamp(0, 1.5),
+              textScaleFactor: mediaquery.textScaleFactor.clamp(1.0, 1.2),
             ),
             child: child,
           );
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
+import 'dart:io';
+
+import 'package:device_preview_screenshot/device_preview_screenshot.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_sharez/app/app.dart';
 import 'package:flutter_sharez/bootstrap.dart';
+import 'package:path_provider/path_provider.dart';
 
 /// This entry point should be used for production only
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   ///You can override your environment variable in bootstrap method here for providers
-  bootstrap(() => const App());
+  final path = (await getDownloadsDirectory())?.path ?? "/";
+  bootstrap(
+    () => DevicePreview(
+      enabled: !kReleaseMode,
+      tools: [
+        ...DevicePreview.defaultTools,
+        DevicePreviewScreenshot(
+            onScreenshot: screenshotAsFiles(Directory(path))),
+      ],
+      builder: (context) => const App(),
+    ),
+  );
 }
