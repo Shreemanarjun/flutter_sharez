6b34e785a934bbe117d8d4b5fcea06cdb3518ba3
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-09-05 22:36:45 +0530

    ✨ add auto update on home page

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -1,15 +1,28 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sharez/core/router/router.gr.dart';
+import 'package:flutter_sharez/core/router/router_pod.dart';
+import 'package:flutter_sharez/features/update_app_version/controller/check_update_available.dart';
 
 @RoutePage(
   deferredLoading: true,
 )
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(
+      checkUpdateAvailablePod,
+      (previous, next) {
+        if (next.value != null) {
+          ref
+              .read(autorouterProvider)
+              .navigate(ChangelogRoute(updateModel: next.value));
+        }
+      },
+    );
     return AutoTabsScaffold(
       routes: const [
         SendRoute(),
