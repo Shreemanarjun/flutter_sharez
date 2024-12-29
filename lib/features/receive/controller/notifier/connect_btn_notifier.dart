539505a1b0d2187e07a636e20b6c53872fea4430
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2024-12-29 23:50:16 +0530

    🏗️  Fix numerous bugs for build

--- lib/features/receive/controller/notifier/connect_btn_notifier.dart
+++ lib/features/receive/controller/notifier/connect_btn_notifier.dart
@@ -5,7 +5,7 @@ import 'package:flutter_sharez/data/model/sender_model.dart';
 import 'package:flutter_sharez/data/service/receiver/receiver_service_pod.dart';
 import 'package:flutter_sharez/features/receive/state/connect_btn_state_pod.dart';
 import 'package:flutter_sharez/shared/riverpod_ext/cancel_extensions.dart';
-import 'package:network_discovery/network_discovery.dart';
+
 
 class ConnectBtnNotifier
     extends AutoDisposeFamilyAsyncNotifier<ConnectBtnState, SenderModel> {
@@ -18,11 +18,11 @@ class ConnectBtnNotifier
     state = AsyncData(ConnectingState());
     state = await AsyncValue.guard(() async {
       final receiverService = ref.watch(receiverServicePod(arg));
-      final currentIP = await NetworkDiscovery.discoverDeviceIpAddress();
+     // final currentIP = await NetworkDiscovery.discoverDeviceIpAddress();
       final result = await receiverService.connectToDevice(
         ip: arg.ip.toString(),
         port: arg.port.toString(),
-        currentIP: currentIP,
+        currentIP: "currentIP",
         cancelToken: ref.cancelToken(),
       );
       return result.when((isAccepted) {
