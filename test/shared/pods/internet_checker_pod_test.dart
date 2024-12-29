6dc393da041bc824043cca12874fcf7aa8dee26a
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2024-12-30 02:26:20 +0530

    ✅ Update test coverage to 100%

--- test/shared/pods/internet_checker_pod_test.dart
+++ test/shared/pods/internet_checker_pod_test.dart
@@ -1,106 +1,101 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sharez/shared/pods/internet_checker_pod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:mocktail/mocktail.dart';
 
-import 'package:flutter_sharez/shared/pods/internet_checker_pod.dart';
-
+class TestInternetConnection extends Mock implements InternetConnection {}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  group(
-    'Internet checker test',
-    () {
-      test(
-        'if internt checker disabled then by deafult internetstatus should be connected',
-        () async {
-          final container = ProviderContainer(
-            overrides: [
-              enableInternetCheckerPod.overrideWithValue(
-                false,
-              ),
-            ],
-          );
+  group('Internet checker test', () {
+    test(
+      'if internt checker disabled then by deafult internetstatus should be connected',
+      () async {
+        final container = ProviderContainer(
+          overrides: [
+            enableInternetCheckerPod.overrideWithValue(
+              false,
+            ),
+          ],
+        );
+        expect(
+            container.read(internetConnectionCheckerInstancePod).checkInterval,
+            equals(const Duration(seconds: 5)));
+        addTearDown(container.dispose);
 
-          addTearDown(container.dispose);
+        final status = await container.read(internetCheckerNotifierPod.future);
+        expect(status, InternetStatus.connected);
+      },
+    );
+    test(
+      "if internet checker is enabled then by default internetstatus should be disconnected",
+      () async {
+        final internetConnection = TestInternetConnection();
+        when(() => internetConnection.onStatusChange).thenAnswer(
+          (invocation) => Stream.value(
+            InternetStatus.connected,
+          ),
+        );
+        final container = ProviderContainer(
+          overrides: [
+            enableInternetCheckerPod.overrideWithValue(
+              true,
+            ),
+            internetConnectionCheckerInstancePod
+                .overrideWithValue(internetConnection),
+          ],
+        );
 
-          final status =
-              await container.read(internetCheckerNotifierPod.future);
-          expect(status, InternetStatus.connected);
-        },
-      );
-      test(
-        'if internt checker enabled then by deafult internetstatus should be connected',
-        () async {
-          final container = ProviderContainer(
-            overrides: [
-              enableInternetCheckerPod.overrideWithValue(
-                true,
-              ),
-            ],
-          );
+        addTearDown(container.dispose);
 
-          addTearDown(container.dispose);
+        final status = await container.read(internetCheckerNotifierPod.future);
+        expect(status, InternetStatus.connected);
+      },
+    );
 
-          var status = await container.read(internetCheckerNotifierPod.future);
-          expectLater(
-            status,
-            equals(InternetStatus.connected),
-          );
+    test(
+      'if internet checker enabled then by deafult internetstatus should be connected and on change status it should be disconnected',
+      () async {
+        final internetConnection = TestInternetConnection();
+        when(() => internetConnection.onStatusChange).thenAnswer(
+          (invocation) => Stream.value(
+            InternetStatus.connected,
+          ),
+        );
+        when(() => internetConnection.checkInterval).thenAnswer(
+          (invocation) => Duration(seconds: 5),
+        );
+        final container = ProviderContainer(
+          overrides: [
+            enableInternetCheckerPod.overrideWithValue(
+              true,
+            ),
+            internetConnectionCheckerInstancePod
+                .overrideWithValue(internetConnection),
+          ],
+        );
+        addTearDown(container.dispose);
 
-          container
-              .read(internetConnectionCheckerInstancePod)
-              .onStatusChange
-              .listen((event) {});
-          final newStatus =
-              await container.read(internetCheckerNotifierPod.future);
+        var status = await container.read(internetCheckerNotifierPod.future);
+        expectLater(
+          status,
+          equals(InternetStatus.connected),
+        );
+        container
+            .read(internetConnectionCheckerInstancePod)
+            .onStatusChange
+            .listen((event) {});
 
-          expect(newStatus, InternetStatus.connected);
-          expect(
-              container
-                  .read(internetConnectionCheckerInstancePod)
-                  .checkInterval,
-              equals(const Duration(seconds: 5)));
-          container
-              .read(internetCheckerNotifierPod.notifier)
-              .change(status: InternetStatus.disconnected);
-          expect(container.read(internetCheckerNotifierPod).requireValue,
-              equals(InternetStatus.disconnected));
-        },
-      );
-      test(
-        'if internet checker enabled then by deafult internetstatus should be connected',
-        () async {
-          final container = ProviderContainer(
-            overrides: [
-              enableInternetCheckerPod.overrideWithValue(
-                true,
-              ),
-            ],
-          );
-          addTearDown(container.dispose);
-
-          var status = await container.read(internetCheckerNotifierPod.future);
-          expectLater(
-            status,
-            equals(InternetStatus.connected),
-          );
-          container
-              .read(internetConnectionCheckerInstancePod)
-              .onStatusChange
-              .listen((event) {});
-
-          expect(
-              container
-                  .read(internetConnectionCheckerInstancePod)
-                  .checkInterval,
-              equals(const Duration(seconds: 5)));
-          container
-              .read(internetCheckerNotifierPod.notifier)
-              .change(status: InternetStatus.disconnected);
-          expect(container.read(internetCheckerNotifierPod).requireValue,
-              equals(InternetStatus.disconnected));
-        },
-      );
-    },
-  );
+        expect(
+            container.read(internetConnectionCheckerInstancePod).checkInterval,
+            equals(const Duration(seconds: 5)));
+        container
+            .read(internetCheckerNotifierPod.notifier)
+            .change(status: InternetStatus.disconnected);
+        expect(container.read(internetCheckerNotifierPod).requireValue,
+            equals(InternetStatus.disconnected));
+      },
+    );
+  });
 }
--- test/shared/widgets/app_locale_popup_test.dart
+++ test/shared/widgets/app_locale_popup_test.dart
@@ -2,13 +2,19 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sharez/core/local_storage/app_storage.dart';
+import 'package:flutter_sharez/l10n/l10n.dart';
+import 'package:flutter_sharez/shared/pods/locale_pod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:flutter_sharez/core/local_storage/app_storage_pod.dart';
 import 'package:flutter_sharez/shared/pods/internet_checker_pod.dart';
 import 'package:flutter_sharez/shared/widget/app_locale_popup.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../helpers/pump_app.dart';
+
+class AppStorageMock extends Mock implements AppStorage {}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -24,24 +30,36 @@ void main() {
     testWidgets(
         'renders Applocalepopup within default English should be selected ',
         (tester) async {
+      final appStorage = AppStorageMock();
+      when(() => appStorage.get(key: any(named: "key"))).thenReturn('en');
+
+      final container = ProviderContainer(
+        overrides: [
+          enableInternetCheckerPod.overrideWith(
+            (ref) => false,
+          ),
+          appBoxProvider.overrideWithValue(appBox),
+          appStorageProvider.overrideWithValue(appStorage),
+        ],
+      );
       await tester.pumpApp(
-        ProviderScope(
-          overrides: [
-            enableInternetCheckerPod.overrideWith(
-              (ref) => false,
-            ),
-            appBoxProvider.overrideWithValue(appBox),
-          ],
+        UncontrolledProviderScope(
+          container: container,
           child: const Scaffold(
             body: AppLocalePopUp(),
           ),
         ),
       );
       expect(find.byType(AppLocalePopUp), findsOneWidget);
+      expectLater(
+        container.read(localePod),
+        AppLocalizations.supportedLocales.first,
+      );
       await tester.runAsync(() async {
-        await tester.tap(find.byIcon(Icons.translate));
+        await tester.tap(find.text("English"));
         await tester.pumpAndSettle();
       });
+
       expect(
           find.widgetWithText(
             SelectedLocaleItem,
@@ -67,7 +85,7 @@ void main() {
       );
       expect(find.byType(AppLocalePopUp), findsOneWidget);
       await tester.runAsync(() async {
-        await tester.tap(find.byIcon(Icons.translate));
+        await tester.tap(find.text("English"));
         await tester.pumpAndSettle();
       });
       expect(
@@ -82,7 +100,7 @@ void main() {
       });
       await tester.pumpAndSettle();
       await tester.runAsync(() async {
-        await tester.tap(find.byIcon(Icons.translate));
+        await tester.tap(find.text("Spanish"));
         await tester.pumpAndSettle();
       });
       expect(
